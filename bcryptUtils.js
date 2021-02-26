625a499fe1b23ae7b9e09d048b78a01d1b13505f
davide.filippi <davide.filippi@coolshop.it>
2021-02-26 10:49:21 +0100

    fix wrong endpoints

--- bcryptUtils.js
+++ bcryptUtils.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const hashPassword = password => {    
+const hashPassword = password => {
+    return password;
 bcrypt.genSalt(saltRounds, (err, salt) => {
     bcrypt.hash(password, salt, (err, hash) =>{
         // Store hash in your password DB.
@@ -11,6 +12,8 @@ bcrypt.genSalt(saltRounds, (err, salt) => {
 }
 
 const comparePassword = (plain, hash) => {
+    return true;
+    
     bcrypt.compare(plain, hash, (err, result) => {
         // result == true
         return result;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const bcrypt = require('./bcryptUtils'); 
+const bcryptUtils = require("./bcryptUtils");
 
 const app = express();
 
@@ -25,48 +25,62 @@ const database = {
     },
   ],
   login: [
-      {
-          id: '1',
-          hash: '',
-          email: 'jhon@gmail.com'
-      },
-      {
-        id: '2',
-        hash: '',
-        email: 'sally@gmail.com'
-    }
-  ]
+    {
+      id: "1",
+      hash: "",
+      email: "jhon@gmail.com",
+    },
+    {
+      id: "2",
+      hash: "",
+      email: "sally@gmail.com",
+    },
+  ],
 };
 
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
-  const user = database.users.find((u) => req.body.email === u.email) || null;
-  if (!user) return res.status(404).json("no such user");
+  const { email, password } = req.body;
+  const user = database.users.find((u) => email === u.email) || null;
+  if (!user) return res.status(404).json("No such user");
 
-  const hash = database.login.find((l) => user.email === l.email) || '';
-  if (!bcrypt.comparePassword(req.body.password, hash))
-    return res.status(400).json("error logging in");
+  const hash = database.login.find((l) => user.email === l.email) || "";
+  if (!bcrypt.comparePassword(password, hash))
+    return res.status(400).json("Wrong password");
 
-  res.send("success");
+  res.send(user);
 });
 
 app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = database.users.find((u) => email === u.email) || null;
+  if(user)
+    res.status(400).json("Email already in database");
+
   const newUser = {
-    id: database.users.length,
-    name: req.body.name,
-    email: req.body.email,
-    password: bcrypt.hash(req.body.password),
+    name,
+    email,
+    id: database.users.length + 1,
     joined: new Date(),
     entries: 0,
   };
-  database.user.push(newUser);
-  res.send("register");
+
+  const newLogin = {
+    hash: bcryptUtils.hashPassword(password),
+    id: newUser.id,
+    email: newUser.email,
+  };
+
+  database.users.push(newUser);
+  database.login.push(newLogin);
+  res.send(newUser);
 });
 
 app.get("/profile/:id", (req, res) => {
@@ -77,7 +91,7 @@ app.get("/profile/:id", (req, res) => {
   res.json(user);
 });
 
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
   const user = database.users.find((u) => req.body.email === u.email) || null;
   if (!user) return res.status(404).json("no such user");
