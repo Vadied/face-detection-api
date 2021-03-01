3aeff91c4ed18278a438744fbaae7b650071785c
davide.filippi <davide.filippi@coolshop.it>
2021-03-01 11:44:08 +0100

    refactor with trx

--- bcryptUtils.js
+++ bcryptUtils.js
@@ -1,16 +1,8 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const hashPassword = password =>  bcrypt.hashSync(password);
-
-const comparePassword = (plain, hash) => {
-    return true;
-
-    bcrypt.compare(plain, hash, (err, result) => {
-        // result == true
-        return result;
-    });
-}
+const hashPassword = async password => bcrypt.hashSync(password, saltRounds);
+const comparePassword = async (plain, hash) => bcrypt.compareSync(plain, hash);
 
 module.exports = {
     hashPassword,
--- server.js
+++ server.js
@@ -16,83 +16,83 @@ const db = knex({
 const bcryptUtils = require("./bcryptUtils");
 
 const app = express();
-
-app.use(bodyParser.json());
 app.use(cors());
-
-const getUserByEmail = (email) => db("users").where({ email }).select("*");
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send(database.users);
+  res.send('Connected to Postgres');
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  db("users")
-    .where({ email })
-    .select("*")
-    .then((user) => {
-      if (!user.length) res.status(404).json("No user found");
-      res.json(user[0]);
-    })
-    .catch((e) => res.status(400).json("Error during sign in"));
+  try {
+    const login = await db("login").where("email", "=", email).select("hash");
+
+
+    const isValid = await bcryptUtils.comparePassword(password, login[0].hash);
+    if (!isValid)
+      res.status(400).json("Wrong password or email");
+
+    const users = await db("users").where("email", "=", email).select("*");
+    if (!users[0]) res.status(400).json("Unable to get user");
+
+    res.json(users[0]);
+  } catch (err) {
+    res.status(400).json("Error during sign in");
+  }
 });
 
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  const hash = bcryptUtils.hashPassword(password);
   const newUser = {
     name,
     email,
     joined: new Date(),
   };
+  const hash = bcryptUtils.hashPassword(password);
   const newLogin = {
     hash,
-    email
+    email,
   };
-
-  db.transaction(trx => {
-    trz.insert(newLogin).into('login')
-    .then(loginEmail => {
-      db("users")
-      .insert(newUser)
-      .then(() => res.json(newUser))
-    })
-  })
-  db("users")
-    .returning("*")
-    .insert(newUser)
-    .then((user) => {
-      db("login").returning("*").insert(newLogin);
-    })
-    .catch((e) => res.status(400).json("Error in registration"));
-  res.json(newUser);
+  db.transaction((trx) => {
+    trx("login")
+      .insert(newLogin)
+      .then(async () => {
+        const users = await trx("users").returning("*").insert(newUser);
+        res.json(users[0]);
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => {
+    res.status(400).json("Error during registration");
+  });
 });
 
-app.get("/profile/:id", (req, res) => {
+app.get("/profile/:id", async (req, res) => {
   const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({ id })
-    .then((user) => {
-      if (!user.length) res.status(404).json("No user found");
+  const user = await db.select("*").from("users").where({ id });
 
-      res.json(user[0]);
-    });
+  if (!user.length) res.status(404).json("No user found");
+
+  res.json(user[0]);
 });
 
 app.get("/profiles", (req, res) => {
   res.json(database.users);
 });
 
-app.put("/image", (req, res) => {
+app.put("/image", async (req, res) => {
   const { id } = req.body;
-  db("users")
-  .where("id", "=", id)
-  .increment("entries", 1)
-  .returning('entries')
-  .then(entries => res.json(entries[0]))
-  .catch(err => res.status(400).json("Error entries"));
+  try {
+    const entries = await db("users")
+      .where("id", "=", id)
+      .increment("entries", 1)
+      .returning("entries");
+    res.json(entries[0]);
+  } catch (err) {
+    console.log("image error", err);
+    res.status(400).json("Error entries");
+  }
 });
 
 app.listen(3000, () => {
